826865aee70128a44e8548d2df7e10d11bd54471
divagarnavean6 <110230195+divagarnavean6@users.noreply.github.com>
2025-04-01 22:21:28 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
+NEXT_PUBLIC_DRIZZLE_DB_URL=postgresql://neondb_owner:npg_4TPasep8ZhmV@ep-holy-thunder-a5rgas2a-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require
 const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
 export const db = drizzle(sql,{schema});
